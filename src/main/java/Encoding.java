855d3f6eb255603d5d4a0015b18880e621b76507
NNJAMBI <nkaranja7@gmail.com>
2020-12-05 15:13:16 +0300

    create local variables for the key and inputed text, add the test as well

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,4 +1,8 @@
 public class Encoding {
+    private int mKey;
+    private String mInputText;
     public Encoding(String inputText, int key) {
+        mKey = key;
+        mInputText = inputText;
     }
 }
--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class EncodingTest {
     @Test
     public void checkIfItCreatesInstance() {
-        Encoding testEncoding = new Encoding("NANCY KARANJA, 2");
+        Encoding testEncoding = new Encoding("NANCY KARANJA", 2);
         assertEquals(true, testEncoding instanceof Encoding);
     }
 }
