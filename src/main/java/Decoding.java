3985613a0de02587eb0bdb4344712d360cae4f05
NNJAMBI <nkaranja7@gmail.com>
2020-12-05 21:16:14 +0300

    create variables for key and input text in Decoding class

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,2 +1,10 @@
 public class Decoding {
+    private int mKey;
+    private String mInputText;
+
+    public Decoding(int key, String inputText) {
+        mKey = key;
+        mInputText = inputText;
+    }
+
 }
