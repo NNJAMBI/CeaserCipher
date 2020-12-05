88b6ab4a3b48fa690689d41332012a99388b8b9a
NNJAMBI <nkaranja7@gmail.com>
2020-12-05 17:12:33 +0300

    add and test the responsivenness of the App.java class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println( "Please select an action; Encode, Decode or Exit");
+        String choice = scanner.next();
+        scanner.next();
+    }
 }
