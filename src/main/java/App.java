72fa66b7d1ba2da2897a8de174d19930479ba9d5
NNJAMBI <nkaranja7@gmail.com>
2020-12-05 22:10:58 +0300

    add the method for exiting the application on app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,26 +9,33 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println( "Please select an action; 1. Encode, 2. Decode or 3. Exit");
+        System.out.println("Please select an action; 1. Encode, 2. Decode or 3. Exit");
         int choice = scanner.nextInt();
         scanner.nextLine();
 
-        if(choice == 1) {
+        if (choice == 1) {
             System.out.println("Enter a text");
             String inputText = scanner.nextLine().toUpperCase();
             System.out.println("Enter a key");
             int key = scanner.nextInt();
-            Encoding encoding = new Encoding(inputText,key);
-            System.out.println(String.format("Your encrypted text is: %s",encoding.encode()));
-        }
-        else if(choice == 2){
+            Encoding encoding = new Encoding(inputText, key);
+            System.out.println(String.format("Your encrypted text is: %s", encoding.encode()));
+        } else if (choice == 2) {
             System.out.println("Enter an encrypted text to decrypt");
             String inputText = scanner.nextLine().toUpperCase();
             System.out.println("Enter a key");
             int key = scanner.nextInt();
             Decoding decoding = new Decoding(key, inputText);
             System.out.println(String.format("Your decrypted text is: %s", decoding.decode()));
+        } else if (choice == 3) {
+            System.exit(0);
         }
 
+        else{
+            System.out.println("Input is unkown");
+
+        }
+
+        }
     }
-}
+
