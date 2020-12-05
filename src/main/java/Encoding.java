5037be5641c9bed3ef42d66447fbcb45b5882918
NNJAMBI <nkaranja7@gmail.com>
2020-12-05 16:23:33 +0300

    create validations for integers,strings and empty fiels and their respective tests, all passed

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,13 +1,15 @@
 public class Encoding {
     private int mKey;
     private String mInputText;
+
     //instantiate code
     public Encoding(String inputText, int key) {
         mKey = key;
         mInputText = inputText;
     }
+
     //create a getter method and check if inputText is saved
-    public String getInputText(){
+    public String getInputText() {
         return mInputText;
     }
 
@@ -15,4 +17,24 @@ public class Encoding {
     public int getKey() {
         return mKey;
     }
+
+    public boolean isNotEmpty() {
+        return !mInputText.trim().isEmpty();
+    }
+
+    public boolean isValidInputText() {
+        char[] chars = mInputText.toCharArray();
+
+        for (char c : chars) {
+            if (Character.isLetter(c) && c != ' ') {
+                return true;
+            }
+
+        }
+        return false;
+    }
+      public boolean isValidKey(){
+        return mKey >1 && mKey < 26;
+      }
+
 }
