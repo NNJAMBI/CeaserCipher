eb605b2934725fa1650c769e0a6de12dc6bf72d5
NNJAMBI <nkaranja7@gmail.com>
2020-12-05 14:42:25 +0300

    add the first test for checking it instantiates

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class EncodingTest {
+    @Test
+    public void checkIfItCreatesInstance() {
+        Encoding testEncoding = new Encoding();
+        assertEquals(true, testEncoding instanceof Encoding);
+    }
 }
