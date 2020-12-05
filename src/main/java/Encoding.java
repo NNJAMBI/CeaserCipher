f731c79495347df6f0dcfa84dfebbc0b122cdb38
NNJAMBI <nkaranja7@gmail.com>
2020-12-05 15:43:11 +0300

    create a test for getting key and let it fail

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,11 +1,12 @@
 public class Encoding {
     private int mKey;
     private String mInputText;
+    //instantiate code
     public Encoding(String inputText, int key) {
         mKey = key;
         mInputText = inputText;
     }
-    //create a getter method
+    //create a getter method and check if inputText is saved
     public String getInputText(){
         return mInputText;
     }
--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -2,15 +2,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EncodingTest {
+    Encoding testEncoding = new Encoding("NANCY KARANJA", 2);
     @Test
     public void checkIfItCreatesInstance() {
-        Encoding testEncoding = new Encoding("NANCY KARANJA", 2);
+        //Encoding testEncoding = new Encoding("NANCY KARANJA", 2);
         assertEquals(true, testEncoding instanceof Encoding);
+
+    }
+    @Test
+    public void checkIfSavedInputText() {
+        //Encoding testEncoding = new Encoding("NANCY KARANJA", 2);
+        assertEquals("NANCY KARANJA", testEncoding.getInputText());
     }
 
     @Test
-    public void checkIfSavedInputText() {
-        Encoding testEncoding = new Encoding("NANCY KARANJA", 2);
-        assertEquals("NANCY KARANJA", testEncoding.getInputText());
+    public void checkIfKeyIsSaved() {
+        assertEquals(2, testEncoding.getKey());
     }
 }
+
+
