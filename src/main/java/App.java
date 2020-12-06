04cbd0ddbd82eca3cb2a8ae128a753b70c75b95d
NNJAMBI <nkaranja7@gmail.com>
2020-12-06 13:46:03 +0300

    Add files to be ignored

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please select an action; 1. Encode, 2. Decode or 3. Exit");
+        System.out.println("Please select an action(answer with number 1, 2 or 3); 1. Encode, 2. Decode or 3. Exit");
         int choice = scanner.nextInt();
         scanner.nextLine();
 
@@ -19,6 +19,7 @@ public class App {
             System.out.println("Enter a key");
             int key = scanner.nextInt();
             Encoding encoding = new Encoding(inputText, key);
+            System.out.println(String.format("your input text was: %s", encoding.getInputText()));
             System.out.println(String.format("Your encrypted text is: %s", encoding.encode()));
         } else if (choice == 2) {
             System.out.println("Enter an encrypted text to decrypt");
@@ -26,13 +27,14 @@ public class App {
             System.out.println("Enter a key");
             int key = scanner.nextInt();
             Decoding decoding = new Decoding(key, inputText);
+            System.out.println(String.format("Your input text was: %s", decoding.getInputText()));
             System.out.println(String.format("Your decrypted text is: %s", decoding.decode()));
         } else if (choice == 3) {
             System.exit(0);
         }
 
         else{
-            System.out.println("Input is unkown");
+            System.out.println("Input is unknown");
 
         }
 
